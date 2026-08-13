6d3c998e4533aa1814e964b78739fae3317d20c7
yshgg <01056649000yoo@gmail.com>
2026-08-13 13:05:56 +0900

    fix: make hanja reaction migration repeatable

--- supabase/migrations/202606170001_hanja_writing_sentence_reactions.sql
+++ supabase/migrations/202606170001_hanja_writing_sentence_reactions.sql
@@ -9,11 +9,17 @@ alter table writing_helper.hanja_writing_reactions
   alter column target_sentence_index set not null;
 
 alter table writing_helper.hanja_writing_reactions
+  drop constraint if exists hanja_writing_reactions_target_sentence_index_check;
+
+alter table writing_helper.hanja_writing_reactions
   add constraint hanja_writing_reactions_target_sentence_index_check
   check (target_sentence_index >= 0);
 
 alter table writing_helper.hanja_writing_reactions
   drop constraint if exists hanja_writing_reactions_target_session_id_session_id_reaction_type_key;
+
+alter table writing_helper.hanja_writing_reactions
+  drop constraint if exists hanja_writing_reactions_sentence_like_unique;
 
 alter table writing_helper.hanja_writing_reactions
   add constraint hanja_writing_reactions_sentence_like_unique
